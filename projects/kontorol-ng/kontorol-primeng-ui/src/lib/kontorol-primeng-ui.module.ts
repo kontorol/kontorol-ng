5b6c16cd7976d19fc3fb8c30862b7abfd6242fee
VIDIUN <41060899+VIDIUN@users.noreply.github.com>
2020-02-10 01:30:51 +0330

    REPLACE NEW

--- projects/kontorol-ng/kontorol-primeng-ui/src/lib/kontorol-primeng-ui.module.ts
+++ projects/kontorol-ng/kontorol-primeng-ui/src/lib/kontorol-primeng-ui.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { StickyDatatableHeaderDirective } from './directives/sticky-datatable-header.directive';
 import { DropdownCloseOnScroll } from './directives/dropdown-close-on-scroll';
 import { MenuCloseOnScroll } from './directives/menu-close-on-scroll';
-import { KalturaCommonModule } from '@kaltura-ng/kaltura-common';
+import { KontorolCommonModule } from '@kontorol-ng/kontorol-common';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenuModule } from 'primeng/menu';
 /**
